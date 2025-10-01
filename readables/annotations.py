8a9915e65287f02cab252af50599ffd3b20205cd
Juti Noppornpitak <juti_n@yahoo.co.jp>
2025-10-01 23:29:53 +0700

    Added annotations for code readability

---
+++ readables/annotations.py
@@ -0,0 +1,19 @@
+"""
+A collection of annotations just to imply the stereotype or significate and may provide little
+to no significate additional functionalities.
+"""
+
+
+def experimental(ref):
+    """ Experimental code """
+    return ref
+
+
+def untested(ref):
+    """ Untested code """
+    return ref
+
+
+def deprecated(ref):
+    """ Deprecated code """
+    return ref
